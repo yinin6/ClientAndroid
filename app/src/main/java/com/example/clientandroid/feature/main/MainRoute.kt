091fea991e4e311a73e0d4283baa8e542364faeb
Yin <yzl1154695556@qq.com>
2024-12-25 13:11:18 +0800

    add mainPage

--- app/src/main/java/com/example/clientandroid/feature/main/MainRoute.kt
+++ app/src/main/java/com/example/clientandroid/feature/main/MainRoute.kt
@@ -1,14 +1,18 @@
 package com.example.clientandroid.feature.guide
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.clientandroid.core.design.component.MyNavigationBar
 
 @Composable
 fun MainRoute(){
@@ -17,16 +21,37 @@ fun MainRoute(){
 
 
 @Composable
-fun MainScreen(){
+fun MainScreen(
 
-    var text = "main"
+){
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        MainContent(
+            modifier = Modifier.weight(1f)
+        )
+        MyNavigationBar(
+            modifier = Modifier.height(80.dp)
+        )
 
-    for (i in 0..100){
-        text += "main"
+
+
     }
 
-    Text(text = text)
 
 
+}
 
+@Composable
+fun MainContent(
+    modifier: Modifier = Modifier
+) {
+    Text(
+        "aaaa",
+        modifier = modifier)
+}
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview(): Unit {
+    MainScreen()
 }
