7df63b73d2da5565f0c738f9061fa9ee003546d1
Yin <yzl1154695556@qq.com>
2024-12-25 23:28:43 +0800

    add tab navigation

--- app/src/main/java/com/example/clientandroid/feature/discovery/DiscoveryRoute.kt
+++ app/src/main/java/com/example/clientandroid/feature/discovery/DiscoveryRoute.kt
@@ -1,13 +1,29 @@
 package com.example.clientandroid.feature.guide
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -15,14 +31,70 @@ fun DiscoveryRoute(
 
 ){
     DiscoveryScreen(
-
     )
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DiscoveryScreen(){
-    Text("discovery", modifier = Modifier.padding(16.dp))
+fun DiscoveryScreen(
+    toSearch :() -> Unit = {},
+){
+    Scaffold (
+        topBar = {
+            MyDiscoveryTopBar(toSearch)
+        },
+        containerColor = Color.White
 
+    ){
+        paddingValues ->
+        LazyColumn (modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues))
+            {
+            item {
+                Text(text = "Discovery")
+            }
+        }
 
+    }
+
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun MyDiscoveryTopBar(toSearch: () -> Unit) {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(
+                onClick = { },
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Menu, contentDescription = "Back",
+                    modifier = Modifier.size(30.dp)
+                )
+
+            }
+        },
+        title = {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Discovery",
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = {
+                toSearch()
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Refresh, contentDescription = "Refresh",
+                    modifier = Modifier.size(30.dp)
+                )
+
+            }
+        }
+    )
 }
